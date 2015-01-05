50e72b0d98e94ef45a4fb1a53b8141eeb8183f5f
Ethan Moon <contact@ethanmoon.com>
2015-01-05 09:44:06 -0500

    Initial Commit

---
+++ Hackerati_TimeTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hackerati_TimeTracker
+//
+//  Created by Ethan on 1/5/15.
+//  Copyright (c) 2015 Hackerati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
