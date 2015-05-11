6635ae0b46139713f40e9ca1713b8e46fe25d9fa
Clayton Rieck <cjrieck123@gmail.com>
2015-05-11 11:39:30 -0400

    display simple view controller

--- Trackerati/Trackerati/ViewController.swift
+++ Trackerati/Trackerati/ViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    init()
+    {
+        super.init(nibName: nil, bundle: nil)
+        title = "Trackerati"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    required init(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
     }
-
-
+    
+    override func loadView()
+    {
+        view = UIView(frame: UIScreen.mainScreen().bounds)
+        view.backgroundColor = UIColor.whiteColor()
+        
+        // Set up UI here
+    }
 }
 
