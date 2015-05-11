1cb16334e6373fc4a387395dd2f66825e0c313dc
Clayton Rieck <cjrieck123@gmail.com>
2015-05-11 11:41:46 -0400

    added bridging header

---
+++ Trackerati/Trackerati/Trackerati-BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  Trackerati-BridgingHeader.h
+//  Trackerati
+//
+//  Created by Clayton Rieck on 5/11/15.
+//  Copyright (c) 2015 The Hackerati. All rights reserved.
+//
+
+#ifndef Trackerati_Trackerati_BridgingHeader_h
+#define Trackerati_Trackerati_BridgingHeader_h
+
+
+#endif
